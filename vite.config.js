a09cc55deb89681976cfba63c3e799b7623ec0ab
udithgunasekara <supeshalagunasekara@gmail.com>
2025-05-15 16:47:06 +0530

    fix: try url path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Country-Explorer/' : '/',
   plugins: [
     tailwindcss(),
   ],
